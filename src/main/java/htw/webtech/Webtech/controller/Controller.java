74cdcd3aa37afb804d16f800b5f7d2d6a28254bc
antmyd <antonmydlach@gmail.com>
2026-05-15 12:25:46 +0200

    fix: Projektkonfiguration und Code bereinigt
      - Merge-Konflikt in README.md aufgelöst
      - JDK-Version im CI-Workflow von 17 auf 25 korrigiert
      - Ungültige Spring Boot Starter-Namen durch korrekte ersetzt
      - Redundantes @RequestMapping("/") im Controller entfernt

--- src/main/java/htw/webtech/Webtech/controller/Controller.java
+++ src/main/java/htw/webtech/Webtech/controller/Controller.java
@@ -3,15 +3,14 @@ package htw.webtech.Webtech.controller;
 import htw.webtech.Webtech.model.CardDTO;
 import htw.webtech.Webtech.service.CardService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
 
 public class Controller {
+
     private final CardService cardService;
 
     public Controller(CardService cardService) {
